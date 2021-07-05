d117871a9ae23a665b1d4b9912d78279d7bf4fc0
Erick Oyarce <eoyarce@facele.cl>
2021-07-05 17:04:40 -0400

    Proyecto base

--- PRUEBAS SPRING/social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationService.java
+++ PRUEBAS SPRING/social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationService.java
@@ -1,9 +1,15 @@
 package microservices.book.multiplication.service;
 
 import microservices.book.multiplication.domain.Multiplication;
+import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 
 public interface MultiplicationService {
 	
 	Multiplication createRandomMultiplication();
 
+	/**
+	* @regreso verdadero si el intento coincide con el resultado de la
+	* multiplicación, falso en caso contrario.
+	*/
+	boolean checkAttempt(final MultiplicationResultAttempt resultAttempt);
 }
--- PRUEBAS SPRING/social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationServiceImpl.java
+++ PRUEBAS SPRING/social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationServiceImpl.java
@@ -2,6 +2,7 @@ package microservices.book.multiplication.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import microservices.book.multiplication.domain.Multiplication;
+import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 
 class MultiplicationServiceImpl implements MultiplicationService{
 	
@@ -17,6 +18,13 @@ class MultiplicationServiceImpl implements MultiplicationService{
 		int factorA = randomGeneratorService.generateRandomFactor();
 		int factorB = randomGeneratorService.generateRandomFactor();
 		return new Multiplication(factorA, factorB);
+	}
+	
+	@Override
+	public boolean checkAttempt(final MultiplicationResultAttempt resultAttempt) {
+		return resultAttempt.getResultAttempt() ==
+				resultAttempt.getMultiplication().getFactorA() *
+				resultAttempt.getMultiplication().getFactorB();
 	}
 
 }
